d749f9035630cbc09664b577ac0225fc1b44d2f3
samuelsmith <samuel.smith.hk@gmail.com>
2017-07-10 13:05:05 -0400

    #29 fixing circular dependency

--- main/smsurvey/interface/participant_interface.py
+++ main/smsurvey/interface/participant_interface.py
@@ -1,9 +1,53 @@
+import base64
 import json
 
 from tornado.web import RequestHandler
 
-from smsurvey.core.security.secure import authenticate
 from smsurvey.core.services.survey_service import SurveyService
+from smsurvey.interface.services.plugin_service import PluginService
+
+
+def authenticate(response):
+    auth = response.request.headers.get("Authorization")
+
+    if auth is None:
+        response.set_status(401)
+        response.write('{"status":"error","message":"Missing Authorization header"}')
+        response.flush()
+
+    if auth.startswith("Basic"):
+        base64enc = auth[6:]
+        credentials = base64.b64decode(base64enc).decode()
+        hyphen_index = credentials.find("-")
+        colon_index = credentials.find(":")
+
+        if colon_index is -1 or hyphen_index is -1:
+            response.set_status(401)
+            response.write('{"status":"error","message":"Invalid Authorization header"}')
+            response.flush()
+        else:
+            owner = credentials[:hyphen_index]
+            plugin_id = credentials[hyphen_index + 1: colon_index]
+            token = credentials[colon_index + 1:]
+
+            plugin_service = PluginService()
+
+            if plugin_service.validate_plugin(owner, plugin_id, token):
+                return {
+                    "valid": True,
+                    "owner": owner
+                }
+            else:
+                response.set_status(403)
+                response.write('{"status":"error","message":"Do not have authorization to R/W survey"}')
+                response.flush()
+
+    else:
+        response.set_status(401)
+        response.write('{"status":"error","message":"Invalid Authorization header - no basic"}')
+        response.flush()
+
+    return {"valid": False}
 
 
 class ParticipantHandler(RequestHandler):
