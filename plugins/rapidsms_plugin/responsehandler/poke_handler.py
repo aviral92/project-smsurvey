dadf8ff3973ad6cfdd62475a071cdd057c6979a1
samuelsmith <samuel.smith.hk@gmail.com>
2017-07-31 16:13:12 -0400

    #46 system can now poke plugins to notify them that a survey is waiting

--- plugins/rapidsms_plugin/responsehandler/poke_handler.py
+++ plugins/rapidsms_plugin/responsehandler/poke_handler.py
@@ -6,6 +6,7 @@ import json
 from .app import SurveyStarter
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 token = os.environ.get("SEC_TOKEN")
 owner = os.environ.get("OWNER_NAME")
@@ -13,9 +14,11 @@ domain = os.environ.get("OWNER_DOMAIN")
 url = os.environ.get("SYSTEM_URL")
 
 
+@csrf_exempt
 def handle(request):
-    plugin_id = request.POST.get('plugin_id')
-    survey_id = request.POST.get('survey_id')
+    body = json.loads(request.body.decode())
+    plugin_id = body['plugin_id']
+    survey_id = body['survey_id']
     a = owner + "@" + domain + "-" + plugin_id + ":" + token
 
     b64 = base64.b64encode(a.encode()).decode()
@@ -23,7 +26,7 @@ def handle(request):
         "Authorization": "Basic " + b64
     }
 
-    r = requests.get(url + "instance?survey_id=" + survey_id + "&status=not_started", headers=headers)
+    r = requests.get(url + "instances?survey_id=" + survey_id + "&status=not_started", headers=headers)
     response = json.loads(r.text)
 
     for iid in response['ids']:
